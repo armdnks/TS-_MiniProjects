5f48b8a80d08a7980c515e7b6407c1b3bdc56b7d
armdnks <armdn.ks@gmail.com>
2022-06-17 01:39:17 +0700

    add comment

--- public/projects/mousemove-parallax/app.js
+++ public/projects/mousemove-parallax/app.js
@@ -1,34 +1,38 @@
 "use strict";
+/**
+ * @title Mousemove Parallax
+ * @tuts  https://www.youtube.com/watch?v=dqzZ0SbSgHY
+ */
 const cakeData = [
-    { layer: 13, speed: 3, image: './img/wafer-1.png' },
-    { layer: 12, speed: 4, image: './img/sprinkles-1.png' },
-    { layer: 11, speed: 4, image: './img/choco-1.png' },
-    { layer: 10, speed: 5, image: './img/choco-2.png' },
-    { layer: 9, speed: 6, image: './img/cake-1.png' },
-    { layer: 8, speed: 7, image: './img/choco-3.png' },
-    { layer: 7, speed: 7, image: './img/wafer-3.png' },
-    { layer: 6, speed: 8, image: './img/sprinkles-2.png' },
-    { layer: 5, speed: 9, image: './img/cake-2.png' },
-    { layer: 4, speed: 6, image: './img/choco-4.png' },
-    { layer: 3, speed: 10, image: './img/wafer-2.png' },
-    { layer: 2, speed: 8, image: './img/wafer-4.png' },
-    { layer: 1, speed: 11, image: './img/sprinkles-3.png' },
+    { layer: 13, speed: 3, image: "./img/wafer-1.png" },
+    { layer: 12, speed: 4, image: "./img/sprinkles-1.png" },
+    { layer: 11, speed: 4, image: "./img/choco-1.png" },
+    { layer: 10, speed: 5, image: "./img/choco-2.png" },
+    { layer: 9, speed: 6, image: "./img/cake-1.png" },
+    { layer: 8, speed: 7, image: "./img/choco-3.png" },
+    { layer: 7, speed: 7, image: "./img/wafer-3.png" },
+    { layer: 6, speed: 8, image: "./img/sprinkles-2.png" },
+    { layer: 5, speed: 9, image: "./img/cake-2.png" },
+    { layer: 4, speed: 6, image: "./img/choco-4.png" },
+    { layer: 3, speed: 10, image: "./img/wafer-2.png" },
+    { layer: 2, speed: 8, image: "./img/wafer-4.png" },
+    { layer: 1, speed: 11, image: "./img/sprinkles-3.png" },
 ];
-const cakeContainer = document.querySelector('.cake-container');
+const cakeContainer = document.querySelector(".cake-container");
 cakeData.forEach((item) => {
     const cakeImage = new Image();
     cakeImage.src = item.image;
-    cakeImage.alt = item.image.replace('./img/', '');
+    cakeImage.alt = item.image.replace("./img/", "");
     cakeImage.style.zIndex = `${item.layer}`;
-    cakeImage.setAttribute('data-speed', String(item.speed));
-    cakeImage.classList.add('cake-image');
+    cakeImage.setAttribute("data-speed", String(item.speed));
+    cakeImage.classList.add("cake-image");
     cakeContainer.appendChild(cakeImage);
 });
-const cakeImages = document.querySelectorAll('.cake-image');
-window.addEventListener('mousemove', cakeParallax);
+const cakeImages = document.querySelectorAll(".cake-image");
+window.addEventListener("mousemove", cakeParallax);
 function cakeParallax(e) {
     cakeImages.forEach((image) => {
-        const speed = Number(image.getAttribute('data-speed'));
+        const speed = Number(image.getAttribute("data-speed"));
         const mouseMove = 75;
         const windowCenter = 2.5;
         const w = window.innerWidth * windowCenter;
